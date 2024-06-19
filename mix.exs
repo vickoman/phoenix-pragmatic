b475b18ad128c6ce764aab6073c9a14e7a3dc5d7
Vickoman <vicmandlagasca@gmail.com>
2024-06-18 19:30:31 -0500

    Add number dependency

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule LightViewStudio.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:number, "~> 1.0.5"}
     ]
   end
 
