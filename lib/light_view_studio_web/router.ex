b0106f17ac17ecf662ab25ef9c2c85379036a15a
Vickoman <vicmandlagasca@gmail.com>
2024-06-19 11:35:16 -0500

    Add router for search live

--- lib/light_view_studio_web/router.ex
+++ lib/light_view_studio_web/router.ex
@@ -21,6 +21,7 @@ defmodule LightViewStudioWeb.Router do
     live "/light", LightLive
     live "/license", LicenseLive
     live "/sales-dashboard", SalesDashboardLive
+    live "/search", SearchLive
   end
 
   # Other scopes may use custom stacks.
