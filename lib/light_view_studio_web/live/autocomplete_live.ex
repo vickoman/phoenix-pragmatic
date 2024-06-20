ab25487a898bc33d095ad5e5dfc0185cf67dfcdf
Vickoman <vicmandlagasca@gmail.com>
2024-06-20 01:36:58 -0500

    feat: Add autocomplete live view for store search

---
+++ lib/light_view_studio_web/live/autocomplete_live.ex
@@ -0,0 +1,153 @@
+defmodule LightViewStudioWeb.AutocompleteLive do
+  use LightViewStudioWeb, :live_view
+
+  alias LightViewStudio.Stores
+  alias LightViewStudio.Cities
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket,
+      zip: "",
+      city: "",
+      matches: [],
+      stores: [],
+      loading: false
+      )
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Find a Store</h1>
+    <div id="search">
+
+      <form phx-submit="search">
+        <input type="text" name="zip" value={@zip}
+              placeholder="Enter ZIP code" autofocus autocomplete="off"
+              readonly={@loading} />
+        <button type="submit">
+          <img src="images/search.svg" alt="Search icon" />
+        </button>
+      </form>
+
+      <form phx-submit="city-search" phx-change="suggest-city">
+        <input type="text" name="city" value={@city}
+              placeholder="City"  autocomplete="off"
+              list="matches"
+              phx-debounce="1000"
+              readonly={@loading} />
+        <button type="submit">
+          <img src="images/search.svg" alt="Search icon" />
+        </button>
+      </form>
+
+      <datalist id="matches">
+      <%= for match <- @matches do %>
+        <option value={match}><%= match %></option>
+      <% end %>
+      </datalist>
+
+      <%= if @loading do %>
+        <div class="loader">
+          Loading...
+        </div>
+      <% end %>
+      <div class="stores">
+        <ul>
+          <%=for store <- @stores do %>
+          <li>
+            <div class="first-line">
+              <div class="name">
+                <%= store.name %>
+              </div>
+              <div class="status">
+                <%= if store.open do %>
+                  <span class="open">Open</span>
+                <% else %>
+                  <span class="closed">Closes</span>
+                <% end %>
+              </div>
+            </div>
+            <div class="second-line">
+              <div class="street">
+                <img src="images/location.svg" alt="Location icon" />
+                <%= store.street %>
+              </div>
+              <div class="phone_number">
+                <img src="images/phone.svg" alt="Phone icon" />
+                <%= store.phone_number %>
+              </div>
+            </div>
+          </li>
+          <% end %>
+        </ul>
+      </div>
+    </div>
+    """
+  end
+
+  def handle_event("search", %{"zip" => zip}, socket) do
+    send(self(), {:run_zip_search, zip})
+
+    socket =
+        assign(socket,
+        stores: [],
+        loading: true
+      )
+    {:noreply, socket}
+  end
+
+  def handle_event("city-search", %{"city" => city}, socket) do
+    send(self(), {:run_city_search, city})
+
+    socket =
+        assign(socket,
+        city: city,
+        stores: [],
+        loading: true
+      )
+    {:noreply, socket}
+  end
+
+  def handle_info({:run_city_search, city}, socket) do
+    case Stores.search_by_city(city) do
+      [] ->
+        socket =
+          socket
+          |> put_flash(:info, "No stores matching \"#{city}\"")
+          |> assign(stores: [], loading: false)
+        {:noreply, socket}
+      stores ->
+        socket =
+          socket
+          |> clear_flash()
+          |> assign(stores: stores, loading: false)
+        {:noreply, socket}
+    end
+  end
+
+  def handle_info({:run_zip_search, zip}, socket) do
+    case Stores.search_by_zip(zip) do
+      [] ->
+        socket =
+          socket
+          |> put_flash(:info, "No stores matching \"#{zip}\"")
+          |> assign(stores: [], loading: false)
+        {:noreply, socket}
+      stores ->
+        socket =
+          socket
+          |> clear_flash()
+          |> assign(stores: stores, loading: false)
+        {:noreply, socket}
+
+    end
+  end
+
+  def handle_event("suggest-city", %{"city" => prefix}, socket) do
+    socket =
+      assign(socket,
+      matches: Cities.suggest(prefix)
+    )
+    {:noreply, socket}
+  end
+end
